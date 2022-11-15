84580d6b242d55b3e19db4f8ab6e0e6ea8b24efe
Maajid Al Rasheeth <rasheethalmajith786@gmail.com>
2022-11-16 01:17:17 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// let options = {
+//   strings: ['Front End Developer', 'UI Designer', 'Mern Stack Developer'],
+//   typeSpeed: 100,
+//   backSpeed: 100,
+//   loop: true
+// };
+
+// let typed = new Typed('.typing', options);
