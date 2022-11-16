5d3796139e6a9e8b164e19dfed9b792af37c12be
Maajid Al Rasheeth <rasheethalmajith786@gmail.com>
2022-11-16 15:34:45 +0530

    navbar updated

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.navbar')
+let display = true;
+hamburger.addEventListener('click', function () {
+  if (display == true) {
+    nav.style.display = 'block';
+    display = false;
+  } else {
+    nav.style.display = 'none';
+    display = true;
+  }
+  // if (nav.style.display = 'block') {
+  //   nav.style.display = 'none'
+  // }
+})
 // let options = {
 //   strings: ['Front End Developer', 'UI Designer', 'Mern Stack Developer'],
 //   typeSpeed: 100,
